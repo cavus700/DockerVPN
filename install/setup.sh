e624b7fca53b4c40126079cbf0346a1ea0d446d5
Robin <robin@dellt20.HOME>
2019-01-08 22:34:01 +0100

    Implemented Listing and Revoking of certificates

--- install/setup.sh
+++ install/setup.sh
@@ -55,6 +55,11 @@ function generate_client_cert() {
         cp /etc/openvpn/ca.crt $KEY_DIR
     fi
 
+    if [[ -f $KEY_DIR/$1.key ]];then
+        echo "::: Error: There is already a certificate for: $1"
+        exit 1
+    fi
+
     echo "::: Generating client cert..."
     cd $EASY_RSA_SRV
     if [[ $# == 1 ]];then
@@ -99,6 +104,50 @@ EOF
     echo "::: Generation complete!"
 }
 
+function revoke_client_cert() {
+    cd $EASY_RSA_CA
+
+    if [[ ! -n $1 ]];then
+        echo "::: Error: No name provided to revoke certificate."
+        help
+        exit 1
+    fi
+
+    if [[ ! -f pki/issued/$1.crt ]];then
+        echo "::: Error No certificate found for: $1"
+        exit 1
+    fi
+    echo "::: Revoking certificate for $1..."
+    cd $EASY_RSA_CA
+    ./easyrsa --batch revoke $1
+    echo "::: Updating crl file..."
+    ./easyrsa gen-crl
+
+    rm -rf $KEY_DIR/$1.*
+    rm -rf "$EASY_RSA_SRV/pki/reqs/$1.req"
+    rm -rf "$EASY_RSA_SRV/pki/private/$1.key"
+    rm -rf "$EASY_RSA_CA/pki/issued/$1.crt"
+    rm -rf "$OUTPUT_DIR/$1.ovpn"
+    cp pki/crl.pem /etc/openvpn/crl.pem
+
+    echo "::: Finished! Restart your server to apply changes."
+}
+
+function list_client_certs() {
+    cd $KEY_DIR
+    echo "::: Active certificates for clients :::"
+    echo "---------------------------------------"
+    CLIENTS=()
+    for entry in *.key
+    do
+        file=$(echo "$entry" | cut -d '.' -f 1)
+        if [[ "$file" != "ta" ]];then
+            echo "$file"
+        fi
+    done
+    echo "---------------------------------------"
+}
+
 function init_client_configs() {
     mkdir -p $DIR_CLIENT_CERT/files
     mkdir -p $DIR_CLIENT_CERT/keys
@@ -141,8 +190,13 @@ function help() {
     echo "    mode: [-s | --setup]  Installs the server."
     echo ""
     echo "          [-c | --client] Generate a client certificate"
-    echo "          option: [-n | --name]=NAME"
-    echo "                  [-p | --password]=PASSWORD"
+    echo "          option: [-n | --name]=NAME (Mandatory)"
+    echo "                  [-p | --password]=PASSWORD (Optional)"
+    echo ""
+    echo "          [-r | --revoke] Revoke a client certificate."
+    echo "          option: [-n | --name]=Name (Mandatory)"
+    echo ""
+    echo "          [-l | --list] Show all active certificates."
     echo ""
     echo "Example: ./setup.sh -s -c -n=User1 -p=Password"
     echo "         Install the server and generate a certificate for User1 with password Password"
@@ -153,21 +207,34 @@ function main() {
     VPN_PROTO=$(echo "$VPN_PROTO" | awk '{print tolower($0)}')
     local INSTALL=false
     local GEN_CLIENT=false
+    local REV_CLIENT=false
 
-    # Check arguments
-    for var in "$@"; do
-        case "$var" in
-            -s|--setup)
-                INSTALL=true;;
-            -c|--client)
-                GEN_CLIENT=true;;
-        esac
-    done
-
-    if [[ ${INSTALL} == false && ${GEN_CLIENT} == false ]];then
-        echo "To start the setup script provide at least one parameter: [--install] [--gen-client]"
+    if [[ ! -n $1 ]];then
+        echo "::: Error: No mode set. See help for more details."
+        help
         exit 1
     fi
+
+    # Check arguments
+    case "$1" in
+        -s|--setup)
+            INSTALL=true
+            echo "::: Running setup mode!";;
+        -c|--client)
+            GEN_CLIENT=true
+            echo "::: Running client generation mode!";;
+        -r|--revoke)
+            REV_CLIENT=true
+            echo "::: Running client revokaction mode!";;
+        -l|--list)
+            list_client_certs
+            exit 0;;
+        *)
+            echo "::: Error: Unknown mode $1"
+            help
+            exit 1
+    esac
+
     if [[ ${INSTALL} == true ]]; then
         source ${SETUP_CONF}
 
@@ -182,7 +249,7 @@ function main() {
         echo "::: Installing wget..."
         apt-get install -qq -y wget
         echo "::: Installing expect..."
-        apt-get install -qq -y expect
+        #apt-get install -qq -y expect
         echo "::: Installing openvpn..."
         apt-get install -qq -y openvpn
 
@@ -325,6 +392,27 @@ function main() {
             fi
         fi
     fi
+
+    if [[ ${REV_CLIENT} == true ]];then
+        if [[ ! -n $2 ]];then
+            echo "Error: No name provided for client revocation."
+            exit 1
+        fi
+
+        case $2 in
+            -n=*|--name=*)
+            CLIENT="${2#*=}"
+            shift # past argument=value
+            ;;
+            *)
+            shift      # unknown option
+            ;;
+        esac
+
+        revoke_client_cert $CLIENT
+    fi
+
+    exit 0
 }
 
 main $@
